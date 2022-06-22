eee8500d5f96b3ef8b0d0be8752a9af7d85ffbb0
Loïc <105038676+MayranL@users.noreply.github.com>
2022-06-22 21:34:46 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/floorballapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.floorballapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText edit = findViewById(R.id.EditText);
+        TextView txt = findViewById(R.id.textView);
+
+
+        final Button button = findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String test;
+                test = edit.getText()+edit.getText().toString();
+                txt.setText(test);
+            }
+        });
+    }
+}
