e441a76be96733497af56cf5119f27db2affbc0b
Loïc <105038676+MayranL@users.noreply.github.com>
2022-07-08 15:39:21 +0200

    Création page Accueil + lien entre la page de bienvenue et d'accueil

--- app/src/main/java/com/example/floorballapp/Accueil.java
+++ app/src/main/java/com/example/floorballapp/Accueil.java
@@ -2,27 +2,30 @@ package com.example.floorballapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class Accueil extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.accueil);
 
-        Intent intentAccueil = new Intent(this,Accueil.class);
+        EditText edit = findViewById(R.id.EditText);
+        TextView txt = findViewById(R.id.textView);
 
+        String test = "";
 
         final Button button = findViewById(R.id.button_id);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                startActivity(intentAccueil);
+                String test;
+                test = edit.getText()+edit.getText().toString();
+                txt.setText(test);
             }
         });
     }
