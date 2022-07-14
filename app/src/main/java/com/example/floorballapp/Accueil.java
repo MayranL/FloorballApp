2e747276693a10eb14ec6eef0c0a1fbd5d640ad0
Loïc <105038676+MayranL@users.noreply.github.com>
2022-07-14 14:56:01 +0200

    Ajout image + descriptif feuille joueur Gérer affichage erreur image

--- app/src/main/java/com/example/floorballapp/Accueil.java
+++ app/src/main/java/com/example/floorballapp/Accueil.java
@@ -2,31 +2,31 @@ package com.example.floorballapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class Accueil extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.accueil);
 
-        EditText edit = findViewById(R.id.EditText);
-        TextView txt = findViewById(R.id.textView);
+        TextView nom = findViewById(R.id.nom);
+        TextView prenom = findViewById(R.id.prenom);
+        TextView age = findViewById(R.id.prenom);
+        ImageView identite = findViewById(R.id.photo_identite);
+        //TODO : Gérer problème affichage image
+        TextView simple = findViewById(R.id.simple);
 
-        String test = "";
-
-        final Button button = findViewById(R.id.button_id);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String test;
-                test = edit.getText()+edit.getText().toString();
-                txt.setText(test);
-            }
-        });
+        identite.findViewById(R.id.logo_floorball);
     }
 }
--- app/src/main/java/com/example/floorballapp/MainActivity.java
+++ app/src/main/java/com/example/floorballapp/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.floorballapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,8 +18,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageView logoFloorball = findViewById(R.id.logo_floorball);
         Intent intentAccueil = new Intent(this,Accueil.class);
 
+        logoFloorball.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(intentAccueil);
+            }
+        });
 
         final Button button = findViewById(R.id.button_id);
         button.setOnClickListener(new View.OnClickListener() {
